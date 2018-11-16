edd9c204be12602bc53ca9c23153141aca6cae59
Karim Awad <karim.g.awad@fau.de>
2018-11-16 19:30:09 +0100

    added ABC Meta data to can_to_ros_publisher

--- src/bobtimus_can/src/can_to_ros_publisher.py
+++ src/bobtimus_can/src/can_to_ros_publisher.py
@@ -4,15 +4,20 @@
 Subclasses of this abstract class store values from CAN messages and publish them to ROS topics
 """
 
+from abc import ABCMeta, abstractmethod
+
 
 class CanToRosPublisher:
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
     def publish_values(self):
         """
         publishes all saved values to ROS topics
         """
         pass
 
+    @abstractmethod
     def save_msg_values(self, msg):
         """
         stores received message values in variables
